3c91aa2842d6074158f50ac12e4c720bf5e5fb93
lxmn99 <60390751+lxmn99@users.noreply.github.com>
2020-07-25 17:50:06 +0530

    Update stringDel.java

--- string/src/main/java/framework/string/stringDel.java
+++ string/src/main/java/framework/string/stringDel.java
@@ -2,7 +2,7 @@ package framework.string;
 import java.io.*;
 import java.util.*;
 
-public class stringDel {
+public class StringDel {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
